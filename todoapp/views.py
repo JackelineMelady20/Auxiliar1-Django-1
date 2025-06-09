86613f2914c050085518be9405118aeb86de30c0
Jackeline Melady Peña <jackelinemeladypena07@gmail.com>
2025-06-08 19:32:50 -0500

    Agregacion metodo post

--- todoapp/views.py
+++ todoapp/views.py
@@ -11,3 +11,17 @@ def tareas(request): #the index view
 
     if request.method == "GET":
         return render(request, "todoapp/index.html", {"tareas": mis_tareas, "categorias": categorias})
+    if request.method == "POST":  # revisar si el método de la request es POST
+        if "taskAdd" in request.POST:  # verificar si la request es para agregar una tarea (esto está definido en el button)
+            titulo = request.POST["titulo"]  # titulo de la tarea
+            
+            nombre_categoria = request.POST["selector_categoria"]  # nombre de la categoria
+            categoria = Categoria.objects.get(nombre=nombre_categoria)  # buscar la categoría en la base de datos
+            
+            contenido = request.POST["contenido"]  # contenido de la tarea
+            
+            nueva_tarea = Tarea(titulo=titulo, contenido=contenido, categoria=categoria)  # Crear la tarea
+            nueva_tarea.save()  # guardar la tarea en la base de datos.
+
+            return redirect("/tareas")  # recargar la página.
+
